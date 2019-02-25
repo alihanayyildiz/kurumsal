82bbfca84d0e58df50b6904a5740709cfb79b90d
alihanayyildiz <alihanayyildiz98@gmail.com>
2019-02-25 19:48:46 +0300

    Kurumsal

---
+++ main/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    var counter = function ($this) {
+        var maxNum = Math.abs(parseInt($this.text()));
+        var i = 0;
+        var repeat = maxNum / 50;
+
+        setInterval(function () {
+
+            $this.text(parseInt(i += repeat));
+
+            if (i > maxNum) {
+                $this.text(parseInt(maxNum));
+                return;
+            }
+
+        }, 40);
+    }
+
+    $("#count .num").each(function (index, element) {
+        counter($(element));
+    });
+
+});
+
+
